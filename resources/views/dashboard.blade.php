f2e5c64f39de2d9c83e9bd4092d82187de7ae0d7
Martin <Martin.Gerstman@ivkhk.ee>
2022-12-06 15:15:33 +0200

    Added dashboard and admin panel layout

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+
+    <!-- partial -->
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="row">
+                <div class="col-xl-12 col-sm-6 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="mb-0">Welcome to DashBoard</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+@endsection
---
+++ resources/views/layouts/common/errors.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+
+<!-- partial -->
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title"> Something went wrong! * _ * </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
